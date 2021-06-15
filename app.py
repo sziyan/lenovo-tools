5f2b12d9f8791eebee9ad90f214ca2eda7f5dc23
Seah Zi Yan <sziyan@hotmail.com>
2021-06-15 19:11:58 +0800

    Cleaned code. Added personal DM. Removed CallbackHandler. Added timeout and /cancel

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ bot.
 import config
 import logging
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram import Bot
 import warranty as warranty_script
 import random
 
@@ -28,59 +28,42 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 token = config.token
+bot = Bot(token=config.token)
 print('Bot started successfully.')
 
-WARRANTY, CONVERT_DATE = range(2)
+WARRANTY = range(1)
 import os
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    update.message.reply_text('Hi! Type /startbot to initiate request!')
 
-def start_tool(update,context):
-    #keyboard = [[InlineKeyboardButton('Get Warranty', callback_data='warranty'), InlineKeyboardButton('Convert Dates', callback_data='convert dates')]]
-    keyboard = [[InlineKeyboardButton('Get Warranty', callback_data='warranty')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Select tools to use', reply_markup = reply_markup)
-    return ConversationHandler.END
-
-def tool_select(update, context):
-    query = update.callback_query
-    query.answer()
-    choice = query.data
-    if choice == 'warranty':
-        query.message.reply_text('Please send me your excel file.')
-        return WARRANTY
-    elif choice == 'convert dates':
-        return CONVERT_DATE
-    else:
-        query.message.reply_text('Error selecting choices')
+def ask_input(update, context):
+    update.message.reply_text("Please send me your excel file. /cancel to cancel.")
     return WARRANTY
 
 def warranty(update, context):
     chat_id = update.message.chat_id
+    mytext = 'Baby used bot.'
     file = update.message.document.get_file().download(custom_path='input.xlsx')
     update.message.reply_text('File received successfully! \n'
                               'Please wait while I process your spreadsheet.')
     warranty_script.main(chat_id)
     update.message.reply_text('Your spreadsheet is ready!')
     update.message.reply_document(document=open('result.xlsx', 'rb'))
+    bot.send_message(chat_id=config.mychat_id,text=mytext)
     os.remove('result.xlsx')
     os.remove('input.xlsx')
-    #encouragement_list = ['Are you sleepy yet?', 'Aiseh doing work ah?', 'Talk to me if you bored ', 'Dont forget I am always here for you.', 'Jiayou Jiayou', 'I love you oh']
-    #random_int = random.randint(0, len(encouragement_list)-1)   #generate random integers
-    #encouragement_reply_text = encouragement_list[random_int] #select random text from encouragement list to send
-    #update.message.reply_text(encouragement_reply_text) #send encouragement text
     return ConversationHandler.END
 
-def convert_dates(update, context):
-    return ConversationHandler.END
+
+def timeout(update, context):
+    update.message.reply_text('Request timed out! /startbot to initiate request again.')
 
 def cancel(update, context):
-    update.message.reply_text('Bye! I hope we can talk again some day.')
-
+    update.message.reply_text('Request terminated. /startbot to initate request again.')
     return ConversationHandler.END
 
 def main():
@@ -94,19 +77,18 @@ def main():
     dp = updater.dispatcher
 
     conv_handler = ConversationHandler(
-        entry_points=[CallbackQueryHandler(tool_select)],
+        entry_points=[CommandHandler('startbot', ask_input)],
         states={
             WARRANTY: [MessageHandler(Filters.document.mime_type('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'), warranty)],
-            CONVERT_DATE: [MessageHandler(Filters.all, convert_dates)],
-            #FILE: [MessageHandler(Filters.text, output_file)]
+            ConversationHandler.TIMEOUT: [MessageHandler(Filters.text, timeout)]
         },
+        conversation_timeout = 60.0,
         fallbacks=[CommandHandler("cancel", cancel)]
 
     )
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler('startbot', start_tool))
     dp.add_handler(conv_handler)
 
     # Start the Bot
